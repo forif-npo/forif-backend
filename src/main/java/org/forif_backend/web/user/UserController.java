bab38a73f273d70ba725cceeb491b3ca653b8881
sjounng <jwsong5160@gmail.com>
2025-12-19 14:35:54 +0900

    [FOR-52] UserController 오류 수정

--- src/main/java/org/forif_backend/web/user/UserController.java
+++ src/main/java/org/forif_backend/web/user/UserController.java
@@ -12,14 +12,9 @@ import org.forif_backend.application.user.UserApplyService;
 import org.forif_backend.application.user.UserService;
 import org.forif_backend.application.user.dto.*;
 import org.forif_backend.common.auth.JwtProvider;
-import org.forif_backend.application.user.dto.UserApplyInfo;
 import org.forif_backend.common.dto.response.ApiResponse;
 import org.forif_backend.web.user.dto.*;
 import org.springframework.http.HttpHeaders;
-import org.forif_backend.common.type.SortDirection;
-import org.forif_backend.domain.user.UserApplyStatus;
-import org.forif_backend.web.user.dto.StudyApplyRequest;
-import org.forif_backend.web.user.dto.UserApplyResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.StringUtils;
@@ -172,25 +167,6 @@ public class UserController {
             return authorization.substring(7);
         }
         return null;
-    }
-
-    @PostMapping("/apply")
-    public ResponseEntity<ApiResponse<Void>> applyStudy(@AuthenticationPrincipal Long userId,
-                                                        @Valid @RequestBody StudyApplyRequest studyApplyRequest) {
-        userApplyService.applyStudy(userId, studyApplyRequest);
-        return ResponseEntity.ok(ApiResponse.successWithMsg("Success"));
-    }
-
-    @GetMapping("/apply/{studyId}")
-    public ResponseEntity<ApiResponse<List<UserApplyResponse>>> getUserApply(@AuthenticationPrincipal Long userId,
-                                                                             @PathVariable("studyId") Integer studyId,
-                                                                             @RequestParam(value = "page", required = false, defaultValue = "0") Long page,
-                                                                             @RequestParam(value = "pageSize", required = false, defaultValue = "20") Long pageSize,
-                                                                             @RequestParam(value = "statusFilter", required = false) UserApplyStatus userApplyStatus,
-                                                                             @RequestParam(value = "applyDateDirection", required = false, defaultValue = "DESC") SortDirection sortDirection) {
-        List<UserApplyInfo> applyInfo = userApplyService.getApplyInfo(userId, studyId, page, pageSize, userApplyStatus, sortDirection);
-        List<UserApplyResponse> response = applyInfo.stream().map(UserApplyResponse::toUserApplyResponse).toList();
-        return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     /**
