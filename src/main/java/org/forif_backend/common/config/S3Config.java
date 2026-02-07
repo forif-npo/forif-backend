e81f8a4d12e226d47ab44736a798d6f6bc984e83
poqo <zxvm5962@naver.com>
2026-02-07 17:10:40 +0900

    [FOR-68] S3 key 기본값 설정 및 환경 구분

--- src/main/java/org/forif_backend/common/config/S3Config.java
+++ src/main/java/org/forif_backend/common/config/S3Config.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,10 +14,10 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-    @Value("${spring.cloud.aws.credentials.access-key}")
+    @Value("${spring.cloud.aws.credentials.access-key:}")
     private String accessKey;
 
-    @Value("${spring.cloud.aws.credentials.secret-key}")
+    @Value("${spring.cloud.aws.credentials.secret-key:}")
     private String secretKey;
 
     @Value("${spring.cloud.aws.region.static:ap-northeast-2}")
@@ -39,5 +41,14 @@ public class S3Config {
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
+    }
+
+    private AwsCredentialsProvider getCredentialsProvider() {
+        // 2. 키가 비어있다면(release 환경), 자동으로 EC2의 IAM 역할을 쓰게 합니다.
+        if (accessKey == null || accessKey.isEmpty()) {
+            return DefaultCredentialsProvider.create();
+        }
+        // 3. 키가 있다면(dev 환경), 그 키를 사용하여 인증합니다.
+        return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
     }
 }
