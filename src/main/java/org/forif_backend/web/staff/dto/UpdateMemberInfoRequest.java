4613140f150f8928f045fbcb000da9e3f342606d
donghee290 <eehd1127@naver.com>
2026-09-08 23:20:36 +0900

    jsonalis 추가

--- src/main/java/org/forif_backend/web/staff/dto/UpdateMemberInfoRequest.java
+++ src/main/java/org/forif_backend/web/staff/dto/UpdateMemberInfoRequest.java
@@ -1,5 +1,6 @@
 package org.forif_backend.web.staff.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Positive;
@@ -8,7 +9,7 @@ import org.hibernate.validator.constraints.Length;
 public record UpdateMemberInfoRequest(
         @Positive Long departmentId,
         @Length(max = 50) String department,
-        @NotBlank @Length(max = 20) String phoneNum
+        @JsonAlias("phoneNum") @NotBlank @Length(max = 20) String phoneNum
 ) {
     @AssertTrue(message = "학과를 선택해주세요.")
     public boolean hasDepartmentSelection() {
